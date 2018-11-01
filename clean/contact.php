6555a04ad22842fe7b1693a533d9d96b3b1b5053
rebyers <42394121+rebyers@users.noreply.github.com>
2018-11-01 16:53:02 -0700

    Adds initial application files

---
+++ clean/contact.php
@@ -0,0 +1,32 @@
+<?php include 'header.php'?>
+
+<?php
+    
+    if(isset($_POST['Name']))
+    {//data is submitted show it
+        //echo $_POST['FirstName'];
+        
+        $to      = 'robert.byers@seattlecentral.edu';
+        $subject = 'Clean Contact Page';
+        $message = 'hello from ' . $_POST['Name'];
+        $replyTo = 'robert.e.byers@gmail.com';
+        $headers = 'From: noreply@example.com' . PHP_EOL . 
+        'Reply-To: ' .$replyTo . PHP_EOL . 
+        'X-Mailer: PHP/' . phpversion();
+        mail($to, $subject, $message, $headers);        
+        echo '<p>Thanks for contacting us!</p>';
+        
+    }else{//show a form
+        echo '
+    <form action="" method="post">
+    Name: <input type="text" name="Name" /><br />
+    Email: <input type="email" name="Email" /><br />
+    <input type="submit" />
+    </form>
+    ';
+}
+
+    
+?>
+
+<?php include 'footer.php'?>
